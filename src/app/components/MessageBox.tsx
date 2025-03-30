4b0bbccda6050362ca95de4f074152766cabc250
paopeaw <paopeaw8246@gmail.com>
2025-03-30 22:44:30 +0700

    prototype

---
+++ src/app/components/MessageBox.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { useRef } from "react";
+// import Message from "./Message";
+
+const MessageBox = () => {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default MessageBox;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,78 @@
+'use client'
 import Image from "next/image";
+import { useRef, useState, useEffect } from "react";
+import { Message } from '../app/components/Message'
+import messageData from '../../public/message.json';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  let messageCount = 40;
+  const [messages, setMessages] = useState<Message[]>([]);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [currentMessageIndex, setCurrentMessageIndex] = useState<number>(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+  useEffect(() => {
+    setMessages(messageData as Message[]);
+  }, []);
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      audioRef.current?.pause();
+      setIsPlaying(false);
+    } else {
+      audioRef.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const handleNextMessage = () => {
+    setCurrentMessageIndex((prevIndex) =>
+      (prevIndex + 1) % messages.length
+    )
+  }
+  const handlePrevMessage = () => {
+    setCurrentMessageIndex((prevIndex) => prevIndex === 0 ? 0 : prevIndex - 1);
+  }
+
+  useEffect(() => {
+    console.log(audioRef.current)
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.src = messages[currentMessageIndex]?.audioPath || "";
+      audioRef.current.load();
+      audioRef.current.currentTime = 0;
+      if (isPlaying) {
+        audioRef.current.play();
+      }
+    }
+  }, [currentMessageIndex, messages, isPlaying]);
+
+  return (
+    <main>
+      {
+        messages.length > 0 ? (
+          messages.map((message) => (
+            <MessageBox key={message.id} message={message} />
+          ))
+        ) : (
+          <p>No messages available.</p> // Fallback when no messages are available
+        )
+      }
+      <h1>{currentMessageIndex}</h1>
+      <audio ref={audioRef} />
+      <button onClick={handleNextMessage}>Next</button>
+      <button onClick={handlePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
+      <button onClick={handlePrevMessage}>Previous</button>
+
+    </main>
+  );
+}
+
+function MessageBox({ message }: { message: Message }) {
+  return (
+    <div className="message-box">
+      <p>{message.message}</p>
     </div>
   );
 }
